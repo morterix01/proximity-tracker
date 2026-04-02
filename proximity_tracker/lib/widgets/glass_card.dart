54ec7449a76f0320a8b7d59962dccf4a519c0f9a
Luis <luis2@example.com>
2026-04-02 20:49:47 +0200

    Fix UI render crash resulting in invisible settings screen

--- proximity_tracker/lib/widgets/glass_card.dart
+++ proximity_tracker/lib/widgets/glass_card.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 
 class GlassCard extends StatelessWidget {
   final Widget child;
-  final double width;
-  final double height;
+  final double? width;
+  final double? height;
   final EdgeInsetsGeometry padding;
 
   const GlassCard({
     super.key,
     required this.child,
-    this.width = double.infinity,
-    this.height = double.infinity,
+    this.width,
+    this.height,
     this.padding = const EdgeInsets.all(20),
   });
 
